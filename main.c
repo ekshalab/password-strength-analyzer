d41acdb8f8c8deef535af78cc7a19c4f4af1e5ab
Eksha Patel <eksha@uoguelph.ca>
2026-09-20 18:45:01 -0400

    Implement has_lowercase check

--- main.c
+++ main.c
@@ -28,6 +28,11 @@ int has_uppercase(const char *password) {
 
 // TODO: check if the password contains at least one lowercase letter
 int has_lowercase(const char *password) {
+    for (int i = 0; i <strlen(password); i++) {
+        if (islower(password[i])) {
+            return 1;
+        }
+    }
     return 0;
 }
 
